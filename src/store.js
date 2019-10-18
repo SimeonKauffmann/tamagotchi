cb7890e96061528e348f4d0d162c54b54ee5343a
Jakob <jakob.carlbring@gmail.com>
2019-10-18 10:58:20 +0200

    Pet gets sad if you overfeed it

--- src/store.js
+++ src/store.js
@@ -40,25 +40,25 @@ export default new Vuex.Store({
       this.state.happy += 1
       this.state.credits += 1
     },
-    Feed(state, cost) {
-      if(state.credits < cost){
-        alert('You cant afford that')
-      } else {
-        this.state.credits-= cost
-        let a = Math.floor(Math.random()*3)
-        console.log(a)
-        if(a === 3){
-          alert("Oh no! You fed the tamagotchi rotten food!")
-          setInterval(function(){this.state.poops.push([Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)], 500)})
-          setTimeout(clearInterval(), 30000)
-          if(this.state.happy >= 60){this.state.happy-= 60}else{this.state.happy = 0}
-          if(this.state.hunger >= 60){this.state.hunger-= 60}else{this.state.hunger = 0}
-        } else {
-          if(this.state.happy + 20 > 100){this.state.happy = 100}else{this.state.happy += 20}
-          if(this.state.hunger + cost > 100){this.state.hunger = 100}else{this.state.hunger += cost}
-        }
-      }
-    },
+    // Feed(state, cost) {
+    //   if(state.credits < cost){
+    //     alert('You cant afford that')
+    //   } else {
+    //     this.state.credits-= cost
+    //     let a = Math.floor(Math.random()*3)
+    //     console.log(a)
+    //     if(a === 3){
+    //       alert("Oh no! You fed the tamagotchi rotten food!")
+    //       setInterval(function(){this.state.poops.push([Math.floor(Math.random() * 100), Math.floor(Math.random() * 100)], 500)})
+    //       setTimeout(clearInterval(), 30000)
+    //       if(this.state.happy >= 60){this.state.happy-= 60}else{this.state.happy = 0}
+    //       if(this.state.hunger >= 60){this.state.hunger-= 60}else{this.state.hunger = 0}
+    //     } else {
+    //       if(this.state.happy + 20 > 100){this.state.happy = 100}else{this.state.happy += 20}
+    //       if(this.state.hunger + cost > 100){this.state.hunger = 100}else{this.state.hunger += cost}
+    //     }
+    //   }
+    // },
     // Play() {
     //     if(this.state.hunger = 0){
     //       alert(this.state.petName + ', is too hungry')
@@ -69,14 +69,26 @@ export default new Vuex.Store({
     //     }
         
     //   },
+    Feed(state, cost) {
+      if(state.credits < cost){
+        alert('You cant afford that')
+      } else {
+        this.state.credits -= cost
+          if(this.state.hunger > 85) {this.state.happy -= 20} else {
+          if(this.state.happy + 20 > 100){this.state.happy = 100}else{this.state.happy += 20}
+          if(this.state.hunger + cost > 100){this.state.hunger = 100}else{this.state.hunger += cost}
+      }
+    }
+      console.log(this.state.hunger)
+    },
       Play(state, fun) {
-        if(state.hunger === 0){
+        if(state.hunger < 10){
           alert(this.state.petName + ', is too hungry')
         } else {
           if (fun + this.state.happy > 100){this.state.happy = 100} else {this.state.happy += fun}
           this.state.credits += ((fun * this.state.happy)/100)
+          this.state.hunger -= fun
         }
-        this.state.hunger -= fun
         
       },
     Sleep() {
